c206a72aa44de550555308a1713f93293ea9e4af
CAMILA ALVES FERREIRA <camila.aferreira22@senacsp.edu.br>
2022-02-17 22:12:11 -0300

    variaveis, vetor, matriz

--- index.php
+++ index.php
@@ -1,7 +1,57 @@
 <?php
 
-echo "Estudar mais git!";
+//comentário 
 
-for($i = 0; $i < 10; $i++){
-    echo "Um monte de olá mundo!<br>";
-}
+/*
+Comentário de bloco
+*/
+
+define('XPTO', 'Valor sempre igual');
+
+echo XPTO;
+
+$num = 2;
+
+$var = '<br>' . "Olá mundo! $num";
+
+echo $var;
+
+echo '<br><br><h1>Vamos tomar um café!</h1>';
+
+//var_dump($var);
+
+//phpinfo(); Informações importantes, como variáveis de ambiente
+
+$domingo = 'Domingo';
+$segunda = 'Segunda';
+$terça = 'Terça-Feira';
+$quarta = 'Quarta-Feira';
+$quinta = 'Quinta-Feira';
+$sexta = 'Sexta-Feira';
+$sabado = 'Sábado';
+
+$diaSemana = [ 1 => 'Domingo',  
+               2 => 'Segunda', 
+               3 => 'Terça', 
+               10 => 'Quarta', 
+               3 => 'Quinta', 
+               4 => 'Sexta', 
+               5 => 'Sábado'];
+
+$contatos = ['Carol', 'Rebeca', 'Robson', 'Pedro'];
+
+echo "<pre>";
+var_dump($diaSemana);
+
+echo "<br>Hoje é {$diaSemana[10]}";//interpolação com {}
+echo "<br>Hoje é " . $diaSemana[3];
+
+
+$usuario = [
+           ["NOME" => "Luiz", "SENHA" => 123],
+           ["NOME" => "Fer", "SENHA" => 321],
+           ["NOME" => "Bono", "SENHA" => 627]
+];
+
+var_dump($usuario);
+echo($usuario);
