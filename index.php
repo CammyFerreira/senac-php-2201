f578723b8879fc957096f09394ce181460cbf30e
Camila Alves <91034827+CammyFerreira@users.noreply.github.com>
2022-03-31 16:21:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
+//variáveis em php colocamos o $ na frente 
+//echo - exibe algo na tela
 echo "Estudar mais git!";
 
+//estrutura de repetição 
 for($i = 0; $i < 10; $i++){
     echo "Um monte de olá mundo!<br>";
 }
